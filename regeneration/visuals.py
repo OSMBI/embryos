451a2cae5fc83ce9b57dc961ded6275ac2b26440
ThomasPluck <tompluck@mail.com>
2018-09-28 13:06:23 +0100

    Update visuals.py

--- regeneration/visuals.py
+++ regeneration/visuals.py
@@ -1,7 +1,7 @@
 '''
 @created: 13:17 (EST) Sun 7/29/2018
 
-@last_modified: 10:05 (EST) Sun 09/25/2018
+@last_modified: 07:10 (EST) Sun 09/28/2018
 
 @authors: 
 
@@ -14,38 +14,39 @@ packet classes interacting over a network into vPython visualizations
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import time
 
-#Initialize plot
-def init_plot():
-    plt.ion()
-    fig = plt.figure()
-    ax = Axes3D(fig)
-    plt.axis('off')
-    return fig,ax
 
+class plot3dClass( object ):
+
+    def __init__(self):
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot( 111, projection='3d' )
+        self.surf = self.ax.scatter(1,1,1)
+        # plt.draw() maybe you want to see this frame?
+
+    def drawNow(self,var):
+        self.surf.remove()
+        self.surf = self.ax.scatter(var[0],var[1],var[2],c=var[3])
+        plt.draw()                      # redraw the canvas
+        self.fig.canvas.flush_events()
+        time.sleep(1)
+        
 #Extract information from board for plotting
 def board_2_coords(board):
     axial_2_cart = np.array([[3**0.5,(3**0.5)/2.,0.5],
                              [0,3./2,1/(2*3**0.5)],
                              [0,0,(2./3)**0.5]])
+    #cart_2_axial = np.linalg.inv(axial_2_cart)
     
-    return [(np.matmul(axial_2_cart,np.array(c.i_id).T),
-             c.HeldPackets != []) for c in board.getAllAgents()]
+    first = [tuple(np.matmul(axial_2_cart,np.array(c.i_id).T).tolist()+['red'])\
+            if c.HeldPackets != [] or c.ReceivedPackets != [] else \
+            tuple(np.matmul(axial_2_cart,np.array(c.i_id).T).tolist()+['blue'])\
+             for c in board.getAllAgents()]
     
-#Iterate existing plot
-def iter_plot(board,fig,ax):
-    coords = board_2_coords(board)
+    x = [f[0] for f in first]
+    y = [f[1] for f in first]
+    z = [f[2] for f in first]
+    c = [f[3] for f in first]
     
-    #Plot agents with packets
-    ax.scatter([c[0][0] for c in coords if c[1]],
-               [c[0][1] for c in coords if c[1]],
-               [c[0][2] for c in coords if c[1]],
-               c = 'red')
-    
-    #Plot agents without packets
-    ax.scatter([c[0][0] for c in coords if not c[1]],
-               [c[0][1] for c in coords if not c[1]],
-               [c[0][2] for c in coords if not c[1]],
-               c = 'blue')
-    #Draw
-    fig.canvas.draw()
+    return (x,y,z,c)
